f89799d3f3038b21ab527e65d9ba05fac21e20e5
Adam Góra <adamgora@icloud.com>
2019-12-03 11:37:06 +0100

    Basic implementation of calculator

--- src/ShippingCalculator/WeightBasedShippingCalculator.php
+++ src/ShippingCalculator/WeightBasedShippingCalculator.php
@@ -2,6 +2,7 @@
 
 namespace App\ShippingCalculator;
 
+use Sylius\Component\Core\Model\OrderItemInterface;
 use Sylius\Component\Shipping\Calculator\CalculatorInterface;
 use Sylius\Component\Shipping\Model\ShipmentInterface;
 
@@ -9,10 +10,21 @@ class WeightBasedShippingCalculator implements CalculatorInterface
 {
     public function calculate(ShipmentInterface $subject, array $configuration): int
     {
+        $totalWeight = 0.0;
+
+        /** @var OrderItemInterface $item */
+        foreach ($subject->getOrder()->getItems() as $item) {
+            $totalWeight += $this->getWeight($item);
+        }
     }
 
     public function getType(): string
     {
         return 'weight_based';
+    }
+
+    private function getWeight(OrderItemInterface $item)
+    {
+        return 0;
     }
 }
