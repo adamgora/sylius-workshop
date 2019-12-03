c86cc7ef1e32b9d74c0155483c7aa64aa405aa58
Adam Góra <adamgora@icloud.com>
2019-12-03 11:26:15 +0100

    Added CalculatorInterface and stubs

--- src/ShippingCalculator/WeightBasedShippingCalculator.php
+++ src/ShippingCalculator/WeightBasedShippingCalculator.php
@@ -2,7 +2,19 @@
 
 namespace App\ShippingCalculator;
 
-class WeightBasedShippingCalculator
+use Sylius\Component\Shipping\Calculator\CalculatorInterface;
+use Sylius\Component\Shipping\Model\ShipmentInterface;
+
+class WeightBasedShippingCalculator implements CalculatorInterface
 {
 
+    public function calculate(ShipmentInterface $subject, array $configuration): int
+    {
+        // TODO: Implement calculate() method.
+    }
+
+    public function getType(): string
+    {
+        // TODO: Implement getType() method.
+    }
 }
