4cdc109f6071244edd8591180f4de7034da0c598
Adam Góra <adamgora@icloud.com>
2019-12-03 12:59:21 +0100

    Created resource entity for subcontractors

---
+++ src/Entity/Subcontractor.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Entity;
+
+use Sylius\Component\Resource\Model\ResourceInterface;
+
+class Subcontractor implements ResourceInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function getId()
+    {
+        // TODO: Implement getId() method.
+    }
+}
