ccfe1426d94e3abbe4c0b609b9eebdc241c322fa
Adam Góra <adamgora@icloud.com>
2019-12-03 15:16:35 +0100

    Implementation of log notifier

--- src/Notifier/LogSubcontractorValidationNotifier.php
+++ src/Notifier/LogSubcontractorValidationNotifier.php
@@ -3,10 +3,24 @@
 namespace App\Notifier;
 
 use App\Entity\Subcontractor;
+use Psr\Log\LoggerInterface;
 
 class LogSubcontractorValidationNotifier implements SubcontractorValidationNotifierInterface
 {
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     public function notify(Subcontractor $subcontractor): void
     {
+        $this->logger->info(
+            sprintf(' Subcontractor %s has been validated', $subcontractor->getName())
+        );
     }
 }
