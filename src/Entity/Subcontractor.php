6ef420e8652f26da32f5d8db426c1ef89eece60a
Adam Góra <adamgora@icloud.com>
2019-12-03 13:05:37 +0100

    Generated basic entity structure

--- src/Entity/Subcontractor.php
+++ src/Entity/Subcontractor.php
@@ -2,15 +2,75 @@
 
 namespace App\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
 use Sylius\Component\Resource\Model\ResourceInterface;
 
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="app_subcontractor")
+ */
 class Subcontractor implements ResourceInterface
 {
     /**
-     * @inheritDoc
+     * @var int
+     *
+     * @ORM\Column(type="integer")
+     * @ORM\Id()
+     * @ORM\GeneratedValue(strategy="AUTO")
      */
-    public function getId()
+    private $id;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(type="string")
+     */
+    private $name;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(type="string")
+     */
+    private $email;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
     {
-        // TODO: Implement getId() method.
+        return $this->id;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string|null $name
+     */
+    public function setName(?string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string|null $email
+     */
+    public function setEmail(?string $email): void
+    {
+        $this->email = $email;
     }
 }
