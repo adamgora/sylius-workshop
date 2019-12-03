bd6d31eec288c535a47e873788e25b2517bdba6e
Adam Góra <adamgora@icloud.com>
2019-12-03 14:30:16 +0100

    Created buildForm method for Subcontractor Form Type

--- src/Form/Type/SubcontractorType.php
+++ src/Form/Type/SubcontractorType.php
@@ -3,8 +3,16 @@
 namespace App\Form\Type;
 
 use Sylius\Bundle\ResourceBundle\Form\Type\AbstractResourceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
 
 class SubcontractorType extends AbstractResourceType
 {
-
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, ['label' => 'sylius.ui.name'])
+            ->add('email', TextType::class, ['label' => 'sylius.ui.email'])
+        ;
+    }
 }
