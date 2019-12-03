b690b48c389626b257f3314bdb986409e0ada83b
Adam Góra <adamgora@icloud.com>
2019-12-03 11:27:55 +0100

    Implemented getType metohod

--- src/ShippingCalculator/WeightBasedShippingCalculator.php
+++ src/ShippingCalculator/WeightBasedShippingCalculator.php
@@ -7,14 +7,12 @@ use Sylius\Component\Shipping\Model\ShipmentInterface;
 
 class WeightBasedShippingCalculator implements CalculatorInterface
 {
-
     public function calculate(ShipmentInterface $subject, array $configuration): int
     {
-        // TODO: Implement calculate() method.
     }
 
     public function getType(): string
     {
-        // TODO: Implement getType() method.
+        return 'weight_based';
     }
 }
