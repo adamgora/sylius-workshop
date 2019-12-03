85156496c5ed6aa6e892e9e01b4e6b38eea375da
Adam Góra <adamgora@icloud.com>
2019-12-03 15:40:31 +0100

    Created fixture class for subcontracotrs

---
+++ src/Fixture/SubcontractorFixture.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Fixture;
+
+use Sylius\Bundle\FixturesBundle\Fixture\AbstractFixture;
+
+class SubcontractorFixture extends AbstractFixture
+{
+    public function load(array $options): void
+    {
+    }
+
+    public function getName(): string
+    {
+    }
+}
