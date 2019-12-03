e362a9cbd9983270bd99d7f7be41fb9213e07a90
Adam Góra <adamgora@icloud.com>
2019-12-03 10:18:28 +0100

    Basic context structure

--- src/Context/TimeBasedChannelContext.php
+++ src/Context/TimeBasedChannelContext.php
@@ -2,7 +2,28 @@
 
 namespace App\Context;
 
-class TimeBasedChannelContext
+use Sylius\Component\Channel\Context\ChannelContextInterface;
+use Sylius\Component\Channel\Context\ChannelNotFoundException;
+use Sylius\Component\Channel\Model\ChannelInterface;
+use Sylius\Component\Channel\Repository\ChannelRepositoryInterface;
+
+class TimeBasedChannelContext implements ChannelContextInterface
 {
-    
+    /**
+     * @var ChannelRepositoryInterface
+     */
+    private $channelRepository;
+
+    public function __construct(ChannelRepositoryInterface $channelRepository)
+    {
+        $this->channelRepository = $channelRepository;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getChannel(): ChannelInterface
+    {
+
+    }
 }
