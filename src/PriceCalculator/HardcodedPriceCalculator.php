31f23881918c65cf13e2b0d3c35c8255ca11547d
Adam Góra <adamgora@icloud.com>
2019-12-04 10:34:29 +0100

    Created basic price calculator

---
+++ src/PriceCalculator/HardcodedPriceCalculator.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\PriceCalculator;
+
+use Sylius\Component\Core\Calculator\ProductVariantPriceCalculatorInterface;
+use Sylius\Component\Core\Exception\MissingChannelConfigurationException;
+use Sylius\Component\Core\Model\ProductVariantInterface;
+
+class HardcodedPriceCalculator implements ProductVariantPriceCalculatorInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function calculate(ProductVariantInterface $productVariant, array $context): int
+    {
+        return 1000;
+    }
+}
