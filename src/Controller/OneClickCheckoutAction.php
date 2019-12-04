a7f589fa2ead430750fd50c40ce76ddffbfa1712
Adam Góra <adamgora@icloud.com>
2019-12-04 13:54:02 +0100

    Added persistence and redirect response

--- src/Controller/OneClickCheckoutAction.php
+++ src/Controller/OneClickCheckoutAction.php
@@ -6,12 +6,14 @@ use App\Entity\Customer\Customer;
 use App\Entity\Order\Order;
 use App\Entity\Order\OrderItem;
 use App\Entity\Product\ProductVariant;
+use Doctrine\Common\Persistence\ObjectManager;
 use SM\Factory\FactoryInterface as StateMachineFactoryInterface;
 use Sylius\Component\Core\Context\ShopperContextInterface;
 use Sylius\Component\Core\OrderCheckoutTransitions;
 use Sylius\Component\Core\Repository\ProductVariantRepositoryInterface;
 use Sylius\Component\Order\Modifier\OrderItemQuantityModifierInterface;
 use Sylius\Component\Resource\Factory\FactoryInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -41,6 +43,10 @@ class OneClickCheckoutAction
      * @var StateMachineFactoryInterface
      */
     private $stateMachineFactory;
+    /**
+     * @var ObjectManager
+     */
+    private $objectManager;
 
     public function __construct(
         ProductVariantRepositoryInterface $productVariantRepository,
@@ -48,7 +54,8 @@ class OneClickCheckoutAction
         FactoryInterface $orderItemFactory,
         OrderItemQuantityModifierInterface $orderItemQuantityModifier,
         ShopperContextInterface $shopperContext,
-        StateMachineFactoryInterface $stateMachineFactory
+        StateMachineFactoryInterface $stateMachineFactory,
+        ObjectManager $objectManager
     )
     {
         $this->productVariantRepository = $productVariantRepository;
@@ -57,18 +64,17 @@ class OneClickCheckoutAction
         $this->orderItemQuantityModifier = $orderItemQuantityModifier;
         $this->shopperContext = $shopperContext;
         $this->stateMachineFactory = $stateMachineFactory;
+        $this->objectManager = $objectManager;
     }
 
     public function __invoke(Request $request): Response
     {
         $order = $this->prepareOrder($request);
         $this->setOrderData($order);
+        $this->applyTransitions($order);
+        $this->persist($order);
 
-        $orderCheckoutStateMachine = $this->stateMachineFactory->get($order, OrderCheckoutTransitions::GRAPH);
-        $orderCheckoutStateMachine->apply(OrderCheckoutTransitions::TRANSITION_ADDRESS);
-        $orderCheckoutStateMachine->apply(OrderCheckoutTransitions::TRANSITION_SELECT_SHIPPING);
-        $orderCheckoutStateMachine->apply(OrderCheckoutTransitions::TRANSITION_SELECT_PAYMENT);
-        $orderCheckoutStateMachine->apply(OrderCheckoutTransitions::TRANSITION_COMPLETE);
+        return new RedirectResponse($request->headers->get('referer'));
     }
 
     /**
@@ -111,5 +117,27 @@ class OneClickCheckoutAction
         $order->setChannel($this->shopperContext->getChannel());
         $order->setLocaleCode($this->shopperContext->getLocaleCode());
         $order->setCurrencyCode($this->shopperContext->getCurrencyCode());
+    }
+
+    /**
+     * @param Order $order
+     * @throws \SM\SMException
+     */
+    private function applyTransitions(Order $order): void
+    {
+        $orderCheckoutStateMachine = $this->stateMachineFactory->get($order, OrderCheckoutTransitions::GRAPH);
+        $orderCheckoutStateMachine->apply(OrderCheckoutTransitions::TRANSITION_ADDRESS);
+        $orderCheckoutStateMachine->apply(OrderCheckoutTransitions::TRANSITION_SELECT_SHIPPING);
+        $orderCheckoutStateMachine->apply(OrderCheckoutTransitions::TRANSITION_SELECT_PAYMENT);
+        $orderCheckoutStateMachine->apply(OrderCheckoutTransitions::TRANSITION_COMPLETE);
+    }
+
+    /**
+     * @param Order $order
+     */
+    private function persist(Order $order): void
+    {
+        $this->objectManager->persist($order);
+        $this->objectManager->flush();
     }
 }
