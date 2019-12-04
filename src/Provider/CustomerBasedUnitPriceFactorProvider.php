ba53ac9e84fdeb446af41af91f6a86c138c34ac1
Adam Góra <adamgora@icloud.com>
2019-12-04 11:50:45 +0100

    Changed calculator to use customer and not item

--- src/Provider/CustomerBasedUnitPriceFactorProvider.php
+++ src/Provider/CustomerBasedUnitPriceFactorProvider.php
@@ -3,16 +3,12 @@
 namespace App\Provider;
 
 use App\Entity\Customer\Customer;
-use App\Entity\Order\OrderItem;
 
 class CustomerBasedUnitPriceFactorProvider implements UnitPriceFactorProviderInterface
 {
 
-    public function provide(OrderItem $orderItem): float
+    public function provide(Customer $customer): float
     {
-        /** @var Customer $customer */
-        $customer = $orderItem->getOrder()->getCustomer();
-
         return $customer->isVip() ? 0.9 : 1.0;
     }
 }
