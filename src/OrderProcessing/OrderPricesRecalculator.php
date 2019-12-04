de6cd1266272c2af12866b16f56b598dda936f58
Adam Góra <adamgora@icloud.com>
2019-12-04 11:13:51 +0100

    Copied price recalculator from sylius core

---
+++ src/OrderProcessing/OrderPricesRecalculator.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\OrderProcessing;
+
+use Sylius\Component\Core\Calculator\ProductVariantPriceCalculatorInterface;
+use Sylius\Component\Core\Model\OrderInterface;
+use Sylius\Component\Order\Model\OrderInterface as BaseOrderInterface;
+use Sylius\Component\Order\Processor\OrderProcessorInterface;
+use Webmozart\Assert\Assert;
+
+final class OrderPricesRecalculator implements OrderProcessorInterface
+{
+    /** @var ProductVariantPriceCalculatorInterface */
+    private $productVariantPriceCalculator;
+
+    public function __construct(ProductVariantPriceCalculatorInterface $productVariantPriceCalculator)
+    {
+        $this->productVariantPriceCalculator = $productVariantPriceCalculator;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function process(BaseOrderInterface $order): void
+    {
+        /** @var OrderInterface $order */
+        Assert::isInstanceOf($order, OrderInterface::class);
+        $channel = $order->getChannel();
+
+        foreach ($order->getItems() as $item) {
+            if ($item->isImmutable()) {
+                continue;
+            }
+
+            $item->setUnitPrice($this->productVariantPriceCalculator->calculate(
+                $item->getVariant(),
+                ['channel' => $channel]
+            ));
+        }
+    }
+}
